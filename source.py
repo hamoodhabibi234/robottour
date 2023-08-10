8f8d2308da3545284f27f1855ad9c04c70ae88d1
hamood <98054712+hamoodhabibi234@users.noreply.github.com>
2023-08-10 00:34:39 -0500

    Update source.py

--- source.py
+++ source.py
@@ -3,14 +3,14 @@ import time
 from gpiozero import PhaseEnableRobot, Button
 
 robot = PhaseEnableRobot(left=(11, 12), right=(15, 16))
-hdg90 = Button(13)
-hdg180 = Button(13)
-hdg270 = Button(13)
-hdg360 = Button(13)
-tof1 = Button(18)
-tof2 = Button(18)
-tof3 = Button(18)
-start = Button(22)
+hdg90 = Button(29)
+hdg180 = Button(31)
+hdg270 = Button(33)
+hdg360 = Button(35)
+tof1 = Button(36)
+tof2 = Button(38)
+tof3 = Button(40)
+start = Button(13)
 
 start.wait_for_press(timeout=None)
 start = time.time()
@@ -18,10 +18,9 @@ end = time.time()
 target = 1 #target time here
 
 for i in range(1):
-  robot.forward()
   #enter code below this
 
-  while {end-start}<target:
+  while end - start < target:
     robot.right()
     time.sleep(0.1)
     end = time.time()
