9d4cc43f96508ee9551a1fcf9a3e7ebbde665677
hamood <98054712+hamoodhabibi234@users.noreply.github.com>
2023-08-09 23:41:50 -0500

    Update source.py

--- source.py
+++ source.py
@@ -1,8 +1,13 @@
 import gpiozero
 import time
-from gpiozero import PhaseEnableRobot
+from gpiozero import PhaseEnableRobot, Button
 
-robot = PhaseEnableRobot(left=(17, 18), right=(27, 22))
+robot = PhaseEnableRobot(left=(11, 12), right=(15, 16))
+lspd = Button(26)
+rspd = Button(16)
+hdg = Button(21)
+tof = Button(20)
+start = Button(26)
 
 for i in range(1):
   robot.forward()
