7b894ae69337cd79c3b949ef8e9b38b2bc737dc4
hamood <98054712+hamoodhabibi234@users.noreply.github.com>
2023-08-15 21:45:59 -0500

    FInish arduino.cpp

--- arduino.cpp
+++ arduino.cpp
@@ -6,27 +6,38 @@ VL53L0X sensor;
 // the setup function runs once when you press reset or power the board
 
 void setup() {
-int led = 9;
-  pinMode(Led, OUTPUT);
-  {
-  Serial.begin(9600);
+  int Led = 8;
+  int 25cm = 9;
+  int 75cm = 10;
+  int 125cm = 11;
+  int 175cm = 12;
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
   Wire.begin();
 
   sensor.setTimeout(500);
-  if (!sensor.init())
-  {
-    Serial.println("Failed to detect and initialize sensor!");
-    while (1) {}
-  }
-
-}
 
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(Led, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(Led, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                      // wait for a second
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  if (sensor.readRangeSingleMillimeters() >= 740 && sensor.readRangeSingleMillimeters() <= 760)
+    digitalWrite(10, HIGH);  // turn the LED on (HIGH is the voltage level)
+  if (sensor.readRangeSingleMillimeters() >= 240 && sensor.readRangeSingleMillimeters() <= 260)
+    digitalWrite(9, HIGH);  // turn the LED on (HIGH is the voltage level)
+  if (sensor.readRangeSingleMillimeters() >= 1240 && sensor.readRangeSingleMillimeters() <= 1260)
+    digitalWrite(11, HIGH);  // turn the LED on (HIGH is the voltage level)
+  if (sensor.readRangeSingleMillimeters() >= 1740 && sensor.readRangeSingleMillimeters() <= 1760)
+    digitalWrite(12, HIGH);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(8, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(10);                      // wait for a second
+  digitalWrite(8, LOW);   // turn the LED off by making the voltage LOW
+  delay(10);                      // wait for a second
 }
